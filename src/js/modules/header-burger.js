00bdfae75b1d923c0c7bf16ccab3b2c977f521b9
Kady <74194999+Kady2020@users.noreply.github.com>
2022-12-21 20:09:37 +0300

    completed header & intro

--- src/js/modules/header-burger.js
+++ src/js/modules/header-burger.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  $(".header__burger").click(function (event) {
-    $(".header,.header__burger,.header__menu,.header__sub-list,.header__button").toggleClass("active");
+  $(".burger").click(function (event) {
+    $(".header,.header__body,.logo,.menu,.burger,.menu__link,.menu__icon,.menu__button").toggleClass("active");
     $("body").toggleClass("lock");
   });
 
-  $('.header__sub-link,.header__button').click(function (event) {
-    $(".header,.header__burger,.header__menu,.header__sub-list,.header__button").removeClass('active');
+  $('.menu__link,.menu__icon').click(function (event) {
+    $(".header,.header__body,.logo,.menu,.burger,.menu__link,.menu__icon,.menu__button").removeClass('active');
     $('body').removeClass('lock');
   });
 });
--- src/js/modules/header-scroll.js
+++ src/js/modules/header-scroll.js
@@ -1,5 +1,5 @@
 $(window).scroll(function () {
-  if ($(window).scrollTop() > 160) {
+  if ($(window).scrollTop() > 100) {
     $('.header').addClass('scroll');
   }
   else {
